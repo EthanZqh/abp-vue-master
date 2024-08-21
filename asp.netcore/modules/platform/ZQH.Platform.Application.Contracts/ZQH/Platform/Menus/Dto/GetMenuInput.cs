49778c26d3f475aad673de8eb0e3cd713ca4cd04
Ethan <zqh_2023vip@126.com>
2024-08-22 03:26:49 +0800

    代码管理

---
+++ asp.netcore/modules/platform/ZQH.Platform.Application.Contracts/ZQH/Platform/Menus/Dto/GetMenuInput.cs
@@ -0,0 +1,11 @@
+﻿using LINGYUN.Platform.Routes;
+using Volo.Abp.Validation;
+
+namespace LINGYUN.Platform.Menus
+{
+    public class GetMenuInput
+    {
+        [DynamicStringLength(typeof(LayoutConsts), nameof(LayoutConsts.MaxFrameworkLength))]
+        public string Framework { get; set; }
+    }
+}
---
+++ asp.netcore/modules/platform/ZQH.Platform.Application.Contracts/ZQH/Platform/Menus/Dto/MenuCreateDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LINGYUN.Platform.Menus
+{
+    public class MenuCreateDto : MenuCreateOrUpdateDto
+    {
+        [Required]
+        public Guid LayoutId { get; set; }
+    }
+}
