267f436af6757a195188e53d40d875804277d630
Ethan <zqh_2023vip@126.com>
2024-08-23 03:42:21 +0800

    代码管理

---
+++ asp.netcore/framework/localization/ZQH.Abp.AspNetCore.Mvc.Localization/ZQH/Abp/AspNetCore/Mvc/Localization/GetTextByKeyInput.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Volo.Abp.Validation;
+
+namespace LINGYUN.Abp.AspNetCore.Mvc.Localization
+{
+    public class GetTextByKeyInput
+    {
+        [Required]
+        public string Key { get; set; }
+
+        [Required]
+        public string CultureName { get; set; }
+
+        [Required]
+        public string ResourceName { get; set; }
+    }
+}
---
+++ asp.netcore/migrations/ZQH.MicroService.LocalizationManagement.EntityFrameworkCore/LocalizationManagementMigrationsDbContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace LY.MicroService.LocalizationManagement.EntityFrameworkCore;
+
+public class LocalizationManagementMigrationsDbContextFactory : IDesignTimeDbContextFactory<LocalizationManagementMigrationsDbContext>
+{
+    public LocalizationManagementMigrationsDbContext CreateDbContext(string[] args)
+    {
+        var configuration = BuildConfiguration();
+        var connectionString = configuration.GetConnectionString("Default");
+
+        var builder = new DbContextOptionsBuilder<LocalizationManagementMigrationsDbContext>()
+            .UseSqlServer(connectionString);
+
+        return new LocalizationManagementMigrationsDbContext(builder!.Options);
+    }
+
+    private static IConfigurationRoot BuildConfiguration()
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../ZQH.MicroService.LocalizationManagement.DbMigrator/"))
+            .AddJsonFile("appsettings.json", optional: false);
+
+        return builder.Build();
+    }
+}
